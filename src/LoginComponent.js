755a3bf6abe2be1c9bcd3a1e71eba9abcab36b62
Mark Zepeda <mark@zepeda.co>
2017-09-26 22:27:07 -0700

    getting responses from login server

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Platform, Linking } from 'react-native';
+import { connect } from 'react-redux';
+import { Actions } from './redux';
+import SafariView from 'react-native-safari-view';
 
-export default class LoginComponent extends Component {
+class LoginComponent extends Component {
   constructor(props){
     super(props);
+    this.decodeUser = this.decodeUser.bind(this);
+    this.login = this.login.bind(this);
     this.state = {
       apiDesc: null
     }
   }
   
   componentDidMount(){
+    Linking.addEventListener('url', this.decodeUser);
+    Linking.getInitialURL().then(url => this.decodeUser({url}));
+
     if(this.props.loginRoot === null || this.props.loginRoot === undefined){
       this.setState({apiDesc: {error: { message: "The app may be outdated."}}});
     }else{
@@ -17,6 +26,43 @@ export default class LoginComponent extends Component {
         res => this.setState({apiDesc: res.data}), 
         err => this.setState({apiDesc: {error: {message: `Failed to load login: ${err}`}}})
       );
+    }
+  }
+
+  componentWillUnmount() {
+    Linking.removeEventListener('url', this.decodeUser);
+  };
+
+  decodeUser({ url }){
+    if(!url) return;
+    const [, tokenParamMatch] = url.match(/user=([^#]+)/);
+    let tokenParam = tokenParamMatch && decodeURIComponent(tokenParamMatch);
+    let data = tokenParam ? parseToken(tokenParam) : null;
+
+    if(data){
+      this.props.LoginUser(
+        data.user, 
+        data.token, 
+        this.props.loginLocation, 
+        this.props.defaultLocation, 
+        (...rest) => console.log("Navigate somewhere!", ...rest)
+      );
+    }
+
+    if (Platform.OS === 'ios') {
+      SafariView.dismiss();
+    }
+  };
+
+  login(url){
+    if (Platform.OS === 'ios') {
+      SafariView.show({
+        url: url,
+        fromBottom: true,
+      });
+    }
+    else {
+      Linking.openURL(url);
     }
   }
 
@@ -32,11 +78,12 @@ export default class LoginComponent extends Component {
       return null;
     }
 
+    const login = this.login;
     let loginMethods = this.state.apiDesc.map(loginMethod => {
       let href = this.props.loginRoot + loginMethod.login;
       let name = loginMethod.name.charAt(0).toUpperCase() + loginMethod.name.slice(1);
       loginMethod.properName = name;
-      loginMethod.href = href;
+      loginMethod.login = () => login(href);
       return this.props.mapLoginToElement(loginMethod);
     });
 
@@ -45,3 +92,14 @@ export default class LoginComponent extends Component {
     return wrappedChildren;
   }
 }
+
+function parseToken (token) {
+  try{
+    let data = JSON.parse(token);
+    return data;
+  }catch(SyntaxError){
+    return null;
+  }
+}
+
+export default connect(null, Actions)(LoginComponent);
